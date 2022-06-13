b32ba71ae8cf80f23bbb80482d1274a80a8083c9
snoopy072 <marwene.smine@gmail.com>
2022-06-14 00:16:37 +0200

    first commit

---
+++ snoopi/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button type="button" className="btn btn-primary">
+          Click Me
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
